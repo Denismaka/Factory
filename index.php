c967ea13b59a6be6783f30e0334fff1a5d9ba82a
Denismaka <makadenis370@gmail.com>
2025-04-02 16:56:30 +0100

    Mise en place du Home responsive

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/style.css" />
   <link rel="stylesheet" href="/assets/font/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="/assets/swiper/package/swiper-bundle.min.css">
   <link rel="website icon" href="/assets/images/logo_website.png" />
 </head>
 
@@ -23,7 +24,7 @@
   <!-- ############## -->
   <header>
     <a href="index.php" class="logo">
-      <img src="/assets/images/logo.png" class="img-fluid rounded">
+      <img src="/assets/images/logo.webp" class="img-fluid rounded">
       Factory
     </a>
     <div class="menu-toggle" id="mobile-menu">
@@ -42,11 +43,50 @@
   <!-- ##############  -->
   <!-- Home  -->
   <!-- ##############  -->
+  <section class="home" id="home">
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide slide1">
+          <h1 class="display-4 text-center">Bienvenue sur notre site !<br>
+            Transformez vos idées en réalité avec nos travaux mécaniques de pointe.
+          </h1>
+        </div>
+        <div class="swiper-slide slide2">
+          <h1 class="display-4 text-center">Découvrez nos services<br>
+            Alimentez votre avenir avec nos solutions innovantes en énergie et puissance.
+          </h1>
+        </div>
+        <div class="swiper-slide slide3">
+          <h1 class="display-4 text-center">Optimisez votre production<br>
+            avec notre expertise en raffinerie de pétrole et industrie gazière.
+          </h1>
+        </div>
+        <div class="swiper-slide slide4">
+          <h1 class="display-4 text-center">Innovons ensemble<br>
+            De la recherche chimique à l'industrie pétrolière, nous façonnons l'avenir.
+          </h1>
+        </div>
+      </div>
+      <!-- Ajout des boutons de navigation -->
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+      <!-- Pagination -->
+      <div class="swiper-pagination"></div>
+    </div>
+  </section>
+
 
 
   <!-- ##############  -->
   <!-- About  -->
   <!-- ##############  -->
+  <section>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nihil reiciendis ipsam provident. At corrupti a maiores sapiente, animi recusandae corporis voluptate optio atque voluptas iure quisquam numquam totam et!
+    Ab cupiditate qui doloribus in praesentium recusandae, iusto commodi nihil doloremque minus atque distinctio, officia corrupti optio corporis. Omnis labore qui inventore cumque cum ab dicta sunt non distinctio error.
+    Deserunt debitis at fugit, cum veritatis ab distinctio eveniet cumque omnis dolore totam eum assumenda mollitia aut architecto ipsum animi numquam labore libero a. Facilis corrupti quaerat odio culpa cupiditate.
+    Porro repellendus quaerat quia aliquid exercitationem ipsam quae animi vero, error minus recusandae reiciendis eos eius aut maiores cupiditate sunt placeat fugiat, quod nostrum. Similique laudantium repellendus quas odit exercitationem!
+    Nisi placeat mollitia nostrum molestiae pariatur eligendi natus vitae quasi soluta repellendus, enim, fugit obcaecati, tempora porro esse numquam! Veritatis culpa impedit laboriosam dolor ut fugit commodi. Praesentium, nam porro.
+  </section>
 
 
   <!-- ##############  -->
@@ -78,11 +118,10 @@
 
 
 
-
-
+  <script src="/assets/swiper/package/swiper-bundle.min.js"></script>
   <script src="/assets/scrollreveal/dist/scrollreveal.min.js"></script>
-  <script src="/assets/Js/app.js"></script>
   <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/assets/Js/app.js"></script>
 </body>
 
 </html>

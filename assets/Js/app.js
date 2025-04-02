8830d0f9365b2071faf2dc43f16f472bf86a73ff
Denismaka <makadenis370@gmail.com>
2025-04-02 15:04:40 +0100

    Premier commit & mise en place du Header responsive & du style generale du projet

---
+++ assets/Js/app.js
@@ -0,0 +1,36 @@
+// Gestion du menu burger
+// const mobileMenu = document.getElementById('mobile-menu');
+// const navList = document.getElementById('nav-list');
+
+// mobileMenu.addEventListener('click', () => {
+//   navList.classList.toggle('active');
+// });
+
+// JavaScript pour les effets de la barre de navigation lors du défilement
+// window.addEventListener('scroll', function () {
+//   const header = document.querySelector('header');
+//   header.classList.toggle('sticky', window.scrollY > 0);
+// });
+
+// Bouton de redirection
+// const btn = document.querySelector('.arrow');
+
+// btn.addEventListener('click', () => {
+//   window.scrollTo({
+//     top: 0,
+//     left: 0,
+//     behavior: 'smooth',
+//   });
+// });
+
+// Script ScrollReveal pour les animations au défilement
+// ScrollReveal({
+//   reset: true,
+//   distance: '80px',
+//   duration: 2000,
+//   delay: 200,
+// });
+// ScrollReveal().reveal('.container, .heading', { origin: 'top' });
+// ScrollReveal().reveal('.col-md-4, form', { origin: 'top' });
+// ScrollReveal().reveal('h1, h3, h4, h5, h2', { origin: 'top' });
+// ScrollReveal().reveal('p, .row, .arrow', { origin: 'top' });
---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Factory</title>
+  <link rel="stylesheet" href="/assets/bootstrap-icons/bootstrap-icons.css">
+  <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <link rel="stylesheet" href="/assets/font/fontawesome/css/all.min.css">
+  <link rel="website icon" href="/assets/images/logo_website.png" />
+</head>
+
+<body>
+  <!-- Bouton de redirection -->
+  <div class="arrow">
+    <i class="fas fa-arrow-up icone"></i>
+  </div>
+
+  <!-- ############## -->
+  <!-- Header -->
+  <!-- ############## -->
+  <header>
+    <a href="index.php" class="logo">
+      <img src="/assets/images/logo.png" class="img-fluid rounded">
+      Factory
+    </a>
+    <div class="menu-toggle" id="mobile-menu">
+      <i class="fas fa-bars"></i>
+    </div>
+    <ul class="nav-list" id="nav-list">
+      <li><a href="/index.php#home" class="text-capitalize">home</a></li>
+      <li><a href="/index.php#about" class="text-capitalize">about</a></li>
+      <li><a href="/index.php#services" class="text-capitalize">services</a></li>
+      <li><a href="/index.php#pages" class="text-capitalize">pages</a></li>
+      <li><a href="/index.php#blog" class="text-capitalize">blog</a></li>
+      <li><a href="/index.php#contact" class="text-capitalize">contact</a></li>
+    </ul>
+  </header>
+
+  <!-- ##############  -->
+  <!-- Home  -->
+  <!-- ##############  -->
+
+
+  <!-- ##############  -->
+  <!-- About  -->
+  <!-- ##############  -->
+
+
+  <!-- ##############  -->
+  <!-- Services  -->
+  <!-- ##############  -->
+
+
+  <!-- ##############  -->
+  <!-- Pages  -->
+  <!-- ##############  -->
+
+
+
+  <!-- ##############  -->
+  <!-- Blog  -->
+  <!-- ##############  -->
+
+
+
+  <!-- ##############  -->
+  <!-- Contact  -->
+  <!-- ##############  -->
+
+
+  <!-- ##############  -->
+  <!-- Footer  -->
+  <!-- ##############  -->
+
+
+
+
+
+
+  <script src="/assets/scrollreveal/dist/scrollreveal.min.js"></script>
+  <script src="/assets/Js/app.js"></script>
+  <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
